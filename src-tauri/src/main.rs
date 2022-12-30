ae2052224e3b6cd1fcad18f76084bddfb36ffd83
limingyuebj <limingyuebj@yuanfudao.com>
2022-12-30 12:02:00 +0800

    feat(debug): 打开主窗口后自动开启devtools

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,6 +13,14 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
